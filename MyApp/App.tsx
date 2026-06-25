df8078c060f463480be19aea16e947b03df73467
Alessandro Alzati <alessandro.alzati@edu-its.it>
2026-06-25 12:55:12 +0200

    login screen come prima pagina quando accedi

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }} >
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }} >
         <Stack.Screen
           name="Login"
           component={LoginScreen}
